18f70c688f9b5596fda4d0972473c1f81b3d5a3b
calumgould <calumgould26@gmail.com>
2020-03-27 20:37:36 +0000

    cleaning master

--- server/helpers/dateFormatter.js
+++
@@ -1,8 +0,0 @@
-const formatDates = function(date){
-  if (!date) return;
-  const UTCStartDate = date.split('-')
-  UTCStartDate[1] = UTCStartDate[1] - 1
-  return Date.UTC(...UTCStartDate)
-}
-
-module.exports = formatDates;
