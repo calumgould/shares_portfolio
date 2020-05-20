61034a4715cf53922a75d126281dd6bcbd681bc8
calumgould <calumgould26@gmail.com>
2020-05-20 15:52:34 +0100

    initial mongoDB atlas config

--- server/server.js
+++ server/server.js
@@ -9,16 +9,36 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-MongoClient.connect('mongodb://localhost:27017')
-.then((client) => {
-  const db = client.db('shares-portfolio');
-  const sharesCollection = db.collection('shares');
-  const sharesRouter = createRouter(sharesCollection);
-  app.use('/api/shares', sharesRouter);
-})
-.catch(console.error);
+// MongoClient.connect('mongodb://localhost:27017')
+// .then((client) => {
+//   const db = client.db('shares-portfolio');
+//   const sharesCollection = db.collection('shares');
+//   const sharesRouter = createRouter(sharesCollection);
+//   app.use('/api/shares', sharesRouter);
+// })
+// .catch(console.error);
 
 
-app.listen(3000, function() {
-  console.log(`Listening on port ${ this.address().port }`);
+// app.listen(3000, function() {
+//   console.log(`Listening on port ${ this.address().port }`);
+// });
+
+const CONNECTION_URL = "mongodb+srv://storacle_team:storaclecodeclan@storacle-ejnt3.mongodb.net/test?retryWrites=true&w=majority"
+
+const DATABASE_NAME = "shares-portfolio"
+
+app.listen(3000, () => {
+  MongoClient.connect(CONNECTION_URL, {useUnifiedTopology: true, useNewUrlParser: true}, (error, client) => {
+    if(error) {
+      throw error;
+    }
+    const database = client.db(DATABASE_NAME);
+    const collection = database.collection('shares');
+    const sharesRouter = createRouter(collection);
+    app.use('/api/shares', sharesRouter);
+    console.log(`Connected to ${DATABASE_NAME}`);
+    client.close()
+  });
 });
+
+
